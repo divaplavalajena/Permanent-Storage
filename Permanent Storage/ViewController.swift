c2f4e692b14010ca405f9568af5666a30f97fe43
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-09 09:06:05 -0500

    Added code for permanent storage of userName

--- Permanent Storage/ViewController.swift
+++ Permanent Storage/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSUserDefaults.standardUserDefaults().setObject("Jena", forKey: "name")
+        
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")!
+        
+        print(userName)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
